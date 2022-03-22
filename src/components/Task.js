9646f01c66d08475ba7fb19e02c2151a849e4886
Shea Cronin <shealcronin@gmail.com>
2022-03-22 14:55:35 -0400

    Allow toggle of completed tasks and sort the tasks alphabetically and by completion

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,23 @@
 import '../styles/Task.css';
 
-function Task({ task }) {
+function Task({ task, setTasks }) {
+    function markTaskAsCompleted() {
+        setTasks((prevTasks) => {
+            const newTasks = [...prevTasks];
+            const i = newTasks.indexOf(task);
+            const taskToChange = { ...newTasks[i] };
+            taskToChange.isCompleted = !taskToChange.isCompleted;
+            newTasks[i] = taskToChange;
+            return newTasks;
+        });
+    }
+
     return (
-        <article className="task">
-            <button className="complete-task-btn"></button>
+        <article className={task.isCompleted ? 'task completed-task' : 'task'}>
+            <button
+                className="complete-task-btn"
+                onClick={markTaskAsCompleted}
+            ></button>
             {task.text}
         </article>
     );
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,18 +1,45 @@
 import Task from './Task';
 import '../styles/TaskList.css';
+import { useState } from 'react';
 
-const tasks = [
-    { id: 1, text: 'Take out trash', completed: false },
-    { id: 2, text: 'Do laundry', completed: false },
-    { id: 3, text: 'Grocery shopping', completed: true },
-    { id: 4, text: 'Clean bathroom', completed: false },
+const tasksData = [
+    { id: 1, text: 'Take out trash', isCompleted: false },
+    { id: 2, text: 'Do laundry', isCompleted: false },
+    { id: 3, text: 'Grocery shopping', isCompleted: true },
+    { id: 4, text: 'Clean bathroom', isCompleted: false },
 ];
 
 function TaskList() {
+    const [tasks, setTasks] = useState(tasksData);
+
+    function orderTasks(tasks) {
+        const alphabeticalTasks = orderTasksAlphabetically();
+
+        // Separate completed tasks.
+        const incompleteTasks = alphabeticalTasks.filter(
+            (task) => !task.isCompleted
+        );
+        const completedTasks = alphabeticalTasks.filter(
+            (task) => task.isCompleted
+        );
+
+        return [...incompleteTasks, ...completedTasks];
+
+        function orderTasksAlphabetically() {
+            return tasks.sort((a, b) => {
+                if (a.text > b.text) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            });
+        }
+    }
+
     return (
         <section className="task-list">
-            {tasks.map((task) => (
-                <Task key={task.id} task={task} />
+            {orderTasks(tasks).map((task) => (
+                <Task key={task.id} task={task} setTasks={setTasks} />
             ))}
         </section>
     );
